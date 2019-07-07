297db0c6f06354e4be965597a3b306a33093a8a5
YeongyeonNa <duddus15@kaist.ac.kr>
2019-07-08 08:28:13 +0900

    데이터 구조 바꾸고, 식당 이름 리사이클러뷰

--- app/src/main/java/android/example/cs496/ui/main/TabFragment4.java
+++ app/src/main/java/android/example/cs496/ui/main/TabFragment4.java
@@ -11,6 +11,7 @@ import android.example.cs496.ui.main.fragment1.RecyclerItemClickListener;
 import android.example.cs496.ui.main.fragment1.Tab1Adapter;
 import android.example.cs496.ui.main.fragment1.phonebook.EditPhoneBook;
 import android.example.cs496.ui.main.fragment4.ItemObject;
+import android.example.cs496.ui.main.fragment4.Menu;
 import android.example.cs496.ui.main.fragment4.RestaurantBookActivity;
 import android.example.cs496.ui.main.fragment4.RestaurantMenuAdapter;
 import android.example.cs496.ui.main.fragment4.Tab4Adapter;
@@ -44,7 +45,10 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import static android.example.cs496.ui.main.fragment1.dummyData.refreshData;
 
 
@@ -67,6 +71,7 @@ public class TabFragment4 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        totalArray = new ArrayList();
         context = getActivity();
         mainBackPressCloseHandler = new MainBackPressCloseHandler(getActivity());
         View v = inflater.inflate(R.layout.tab_fragment4,container,false);
@@ -100,10 +105,10 @@ public class TabFragment4 extends Fragment {
             super.onPreExecute();
 
             //진행다일로그 시작
-            progressDialog = new ProgressDialog(context); // 문제 생기면 바로 지우기
-            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progressDialog.setMessage("잠시 기다려 주세요.");
-            progressDialog.show();
+//            progressDialog = new ProgressDialog(context); // 문제 생기면 바로 지우기
+//            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//            progressDialog.setMessage("잠시 기다려 주세요.");
+//            progressDialog.show();
 
         }
         protected Void doInBackground(Void... params) {
@@ -125,14 +130,15 @@ public class TabFragment4 extends Fragment {
                 for(int i=0; i<=mSize-1; i++){
                     String store = storeArray.get(i);
                     String menu = menuArray.get(i);
-                    String[] menuSplited = menu.split(" "); // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 엘리멘트 나누는 함수 필요
+                    String[] menuRefined = refineString(menu);
+                    ItemObject itemObject = makeObject(store, menuRefined);
+                    totalArray.add(itemObject);
 
-                    totalArray.add(new ItemObject(store, menuSplited));
-                    System.out.println(i);
-                    System.out.println(totalArray.get(i).getTitle());
-                    System.out.println(menu);
-                    for(int j=0; j<=totalArray.get(i).getMenus().length -1; j++){
-                        System.out.println(totalArray.get(i).getMenus()[j]);
+                    System.out.println(itemObject.getTitle());
+                    Map<String,ArrayList<Menu>> hm =itemObject.getMenus();
+                    for(Map.Entry<String,ArrayList<Menu>> entry : hm.entrySet()){
+
+                        System.out.println("key : " + entry.getKey() + " , value : " + entry.getValue());
                     }
                 }
             } catch (IOException e) {
@@ -149,8 +155,65 @@ public class TabFragment4 extends Fragment {
             recyclerView.setLayoutManager(layoutManager);
             recyclerView.setAdapter(myAdapter);
 
-            progressDialog.dismiss();
+//            progressDialog.dismiss();
         }
+    }
+    public String[] refineString(String string){
+        string = string.trim();
+        if(string.startsWith("식당에서")){
+            String[] list = new String[1];
+            list[0] ="식당에서 메뉴를 업로드하지 않았습니다.";
+            return  list;
+        }else{
+            string = string.replace("₩ ", "₩");
+            string = string.replace("-마로니애- ", "");
+            string = string.replace("메뉴 ", "메뉴");
+            string = string.replace("0 ","0");
+            string = string.replace("> ", ">");
+            string = string.replace("0원>","0원> ");
+            String[] list = string.split(" ");
+            return  list;
+        }
+    }
+
+    public ItemObject makeObject(String store, String[] list){
+        int nowTag = -1;
+
+        ArrayList<Menu> new_menus = new ArrayList<>();
+        ItemObject result;
+        Map<String, ArrayList<Menu>> map = new HashMap<String, ArrayList<Menu>>();
+        for(int i = 0; i<=list.length-1; i++){
+            if(list[i].startsWith("<")||list[i].startsWith("(한식")||list[i].startsWith("(죽식")){ // 태그 일 때, 다른 태그도 있을 수 있음, 북측의 일품메뉴는 별로 시작함
+                if(nowTag != -1){
+                    String key = list[nowTag];
+                    ArrayList<Menu> value = new_menus;
+                    map.put(key, value);
+                }
+                nowTag = i;
+                new_menus = new ArrayList<>();
+            }
+            else {
+                if(nowTag == -1){
+                    String key = list[i];// 맵에 더하기
+                    Menu menu = new Menu(list[i],0,1,1);
+                    new_menus = new ArrayList<>();
+                    new_menus.add(menu);
+                    map.put(key, new_menus);
+                }
+                else {
+                    new_menus.add(new Menu(list[i],0,1,1));
+                }
+            }
+        }
+        if(nowTag != -1){
+            String key = list[nowTag];
+            ArrayList<Menu> value = new_menus;
+            map.put(key, value);
+        }
+        result = new ItemObject(store, map);
+        return result;
+
+
     }
 
 //    @Override
