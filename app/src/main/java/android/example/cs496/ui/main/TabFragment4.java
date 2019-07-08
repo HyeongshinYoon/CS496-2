e0b91b2eae03be0d436171864960458915e2d60c
HyeongshinYoon <35055625+HyeongshinYoon@users.noreply.github.com>
2019-07-09 05:38:23 +0900

    edit server

--- app/src/main/java/android/example/cs496/ui/main/TabFragment4.java
+++ app/src/main/java/android/example/cs496/ui/main/TabFragment4.java
@@ -87,6 +87,5 @@ public class TabFragment4 extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        resetData();
     }
 }
